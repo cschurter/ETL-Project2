a441a1be19071c43490ba82b416bc990e920860c
student x <carolineschurter@yahoo.com>
2019-01-19 00:52:01 -0800

    added geocoding and google heat map

---
+++ .ipynb_checkpoints/includes-checkpoint.py
@@ -0,0 +1,34 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "gmaps_api_key = 'AIzaSyApcUMzkCy7Eew7eC9t1z6xvFdfHJoj4aw'"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
